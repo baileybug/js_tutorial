074282e2221c37d54ee1a25005f1463ee831ddfd
Bailey <bailey@useaquasuite.com>
2024-08-02 15:41:07 -0700

    update new pages

---
+++ palindrome.js
@@ -0,0 +1,22 @@
+// Reverses a string
+function reverse(string) {
+    return Array.from(string).reverse().join("");
+}
+
+// Returns true for a palindrome, false otherwise
+function palindrome(string) {
+    let processedContent = string.toLowerCase();
+    return processedContent === reverse(processedContent);
+}
+
+
+
+
+
+
+
+
+// Exercise
+//function emailParts(email) {
+    //return email.toLowerCase().split("@");
+//}
